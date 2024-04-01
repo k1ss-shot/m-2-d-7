7d04bf7c8db6bd8417c1e74c425adf13d6e837da
kairat <kairat@mancho.dev>
2024-04-01 19:57:21 +0600

    done task 3

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 
 # # new_list = list(map(lambda x: x*2, my_list))
 
-# from functools import reduce
+from functools import reduce
 
 # product = reduce((lambda x,y: x*y),my_list)
 
@@ -21,10 +21,18 @@
 
 # print(cities_lower)
 
-number_list = [4, 3, 6 ,8, 9]
+# number_list = [4, 3, 6 ,8, 9]
 
-number_list = list(filter(lambda x: x%2 == 0, number_list))
+# number_list = list(filter(lambda x: x%2 == 0, number_list))
 
-number_list = list(map(lambda x: pow(x,3), number_list))
+# number_list = list(map(lambda x: pow(x,3), number_list))
 
-print(number_list)
+# print(number_list)
+
+
+numbers = [21, 32, 2, 122, 32]
+
+numbers = list(filter(lambda x: x%2 != 0, numbers))
+numbers = reduce(lambda x, y: x + y, numbers)
+
+print(numbers)
